b8c36de8f97964df855b9390582745985a28c67d
Lee DaeYeob <tony6303@naver.com>
2021-03-22 12:39:52 +0900

    2.인스타그램 클론 - 시큐리티 설정 완료(Oauth2.0 , cors설정은 나중에)

---
+++ src/main/java/com/cos/costagram/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.cos.costagram.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Bean
+	public BCryptPasswordEncoder encode() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+
+	}
+
+	// 모델 Image, User, Likes, Follow, Tag : 인증 필요함.
+	// auth 주소 : 인증 필요없음.
+	// 
+	// crossorigin 이 먹히지않는다 ? -> controller 진입 하기전에 security 에서 막힘 -> 직접 만들어야함
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/", "/user/**", "/image/**", "/follow/**").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+		.anyRequest()
+		.permitAll()
+		.and()
+		.formLogin()
+		.loginPage("/auth/loginForm")
+		.loginProcessingUrl("/");
+		// OAuth2.0 , CORS 는 나중에
+		
+		
+	}
+	
+	
+	
+}
